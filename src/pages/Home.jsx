b9867ee20282bbfe1d1567920826a87659cb9b4b
iftakhar <iftakharalamshuvo11@gmail.com>
2024-05-11 18:16:15 +0600

    some fixes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import { Helmet } from "react-helmet-async";
 import Banner from "../components/Banner/Banner";
 import Newslatter from "../components/Newslatter/Newslatter";
-import D3 from "../components/3d/D3";
+
 
 
 const Home = () => {
@@ -18,9 +18,7 @@ const Home = () => {
         <div className="container mx-auto">
             <Newslatter></Newslatter>
         </div>
-        <div>
-          <D3></D3>
-        </div>
+        
       </div>
      
     </div>
